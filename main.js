d6030e7bfcd1315f5f30c828f55fd525a1496cf4
Web__Dev <murod.london@gmail.com>
2023-11-19 14:11:16 +0000

    Create Factory functions

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 2,
-  },
-  isVisible: true,
-  draw: function () {
-    console.log('Draw');
-  },
-};
+// Factory Function
 
-circle.draw();
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log('Draw');
+    },
+  };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(4);
+console.log(circle2);
