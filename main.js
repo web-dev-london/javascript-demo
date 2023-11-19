5b532a9a954a7e0e30153f2da25f38b12dfbe553
Web__Dev <murod.london@gmail.com>
2023-11-19 20:24:20 +0000

    Template Literal

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-// String primitive
+// Template literal
 
-const message = ' Hello World! ';
+const message = 'John!';
 
-// String Object
-const another = new String('Hi');
+const another = `Hi ${message} ${1 + 3},
+Thank you for joining my mailing list.
 
-console.log(message.length);
-console.log(message[1]);
-console.log(message.includes('World'));
-console.log(message.includes('John'));
-console.log(message.startsWith('Hello'));
-console.log(message.startsWith('World'));
-console.log(message.endsWith('!'));
-console.log(message.indexOf('World!'));
-console.log(message.replace('World', 'John'));
-console.log(message.toUpperCase());
-console.log(message.toLowerCase());
-console.log(message.trim());
-console.log(message.trimStart());
-console.log(message.trimEnd());
-console.log(message.split(' '));
+Regards, Murat`;
