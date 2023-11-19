1e37c9a6b9cba7110275e8351331985e0c96acae
Web__Dev <murod.london@gmail.com>
2023-11-19 17:14:53 +0000

    Garbage Collection

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// Cloning an Object
-const circle = {
-  radius: 1,
-  draw() {
-    console.log('draw');
-  },
-};
-
-// const another = {};
-// for (let key in circle)
-// another[key] = circle[key];
-
-// const another = Object.assign({ color: 'red' }, circle);
-
-const another = { ...circle };
-
-console.log(another);
+// Garbage Collection
+let circle = {};
+console.log(circle);
