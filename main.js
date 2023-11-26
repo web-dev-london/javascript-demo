59d11d9734a4c23f3ee0adb8fd7b8110f93d4f83
Web__Dev <murod.london@gmail.com>
2023-11-26 19:41:16 +0000

    Practicing Constructor Functions

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-// Factory function
-
-function createCircle(radius) {
-  return {
-    radius,
-    isVisible: true,
-    draw() {
-      return `Hello Draw`;
-    },
-    conor: [1, '2', 3],
-  };
-}
-
-const circle1 = createCircle(2);
-console.log(`Hello Circle`, circle1);
-
-const circle2 = createCircle(4);
-console.log(`Another Circel`, circle2);
-
 // Constructor function
 
 function Circle(radius) {
