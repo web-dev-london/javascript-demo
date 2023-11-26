8b331de987a6d125178f70fa14fbe6e0bebd0140
Web__Dev <murod.london@gmail.com>
2023-11-26 19:40:43 +0000

    Practice Constructor Functions

--- main.js
+++ main.js
@@ -16,3 +16,14 @@ console.log(`Hello Circle`, circle1);
 
 const circle2 = createCircle(4);
 console.log(`Another Circel`, circle2);
+
+// Constructor function
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    return `Hello Murod`;
+  };
+}
+
+const circle5 = new Circle(5);
