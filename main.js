caec139ccc06b7acfa477b76645e6d9993e83164
Web__Dev <murod.london@gmail.com>
2023-11-26 22:39:47 +0000

    Exercise 3. Check Equality and are Same

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const address = createAddress('a', 'b', 'c');
+const address1 = new Address('a', 'b', 'c');
+const address2 = new Address('a', 'b', 'c');
 
-function createAddress(street, city, postCode) {
-  return {
-    street,
-    city,
-    postCode,
-  };
-}
-console.log(createAddress(address));
-
-function CreateAddress(street, city, postCode) {
+function Address(street, city, postCode) {
   this.street = street;
   this.city = city;
   this.postCode = postCode;
 }
 
-const address2 = new CreateAddress(
-  'John',
-  'Mary',
-  'Harry',
-);
-console.log(address2);
+function areEqual(address1, address2) {
+  return (
+    address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.postCode === address2.postCode
+  );
+}
+
+function areSame(address1, address2) {
+  return address1 === address2;
+}
+
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
