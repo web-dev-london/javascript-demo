9264763e551383869fb18af3ff663c01c91ffdcc
Web__Dev <murod.london@gmail.com>
2023-12-03 18:34:17 +0000

    FizzBuzz Exercise

--- main.js
+++ main.js
@@ -1,34 +1,13 @@
-function namesOnly(arr) {
-  return arr.map((num) => {
-    if (num.age >= 17) {
-      return `${num.name} can go to The Matrix`;
-    }
-    if (num.age) {
-      return `${num.name} is under age!`;
-    }
-  });
+function fizzBuzz(input) {
+  if (typeof input !== 'number') return `Not a Number`;
+
+  if (input % 3 === 0 && input % 5 === 0)
+    return `FizzBuzz`;
+
+  if (input % 3 === 0) return `Fizz`;
+
+  if (input % 5 === 0) return `Buzz`;
+  return input;
 }
-console.log(
-  namesOnly([
-    {
-      name: 'Angelina Jolie',
-      age: 80,
-    },
-    {
-      name: 'Eric Jones',
-      age: 2,
-    },
-    {
-      name: 'Paris Hilton',
-      age: 5,
-    },
-    {
-      name: 'Kayne West',
-      age: 16,
-    },
-    {
-      name: 'Bob Ziroll',
-      age: 100,
-    },
-  ]),
-);
+
+fizzBuzz();
