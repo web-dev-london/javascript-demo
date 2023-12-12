2de282d894a827be39ed9e10c89cfbdd7153e13d
Web__Dev <murod.london@gmail.com>
2023-12-12 05:48:53 +0000

    Exercise moving Element

--- main.js
+++ main.js
@@ -1,41 +1,17 @@
-// showStars(5);
+const numbers = [1, 2, 3, 4];
 
-// function showStars(rows) {
-//   for (let i = 1; i <= rows; i++) {
-//     let pattern = '';
-//     for (let y = 0; y < i; y++) pattern += '*';
-//     console.log(`${i} ${pattern}`);
-//   }
-// }
+const output = move(numbers, 0, 5);
 
-// Make an array of the names in h1s, and the ages in h2s
-function readyToPutInTheDOM(array) {
-  return array.map(
-    ({ name, age }) => `<h1>${name}</h1> <h2>${age}</h2>`,
-  );
+console.log(output);
+
+function move(array, index, offset) {
+  const position = index + offset;
+  if (position > array.length || position < 0) {
+    console.error('Invalid error');
+    return;
+  }
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(position, 0, element);
+  return output;
 }
-
-console.log(
-  readyToPutInTheDOM([
-    {
-      name: 'Angelina Jolie',
-      age: 80,
-    },
-    {
-      name: 'Eric Jones',
-      age: 2,
-    },
-    {
-      name: 'Paris Hilton',
-      age: 5,
-    },
-    {
-      name: 'Kayne West',
-      age: 16,
-    },
-    {
-      name: 'Bob Ziroll',
-      age: 100,
-    },
-  ]),
-);
